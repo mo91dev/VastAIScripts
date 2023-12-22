9930eb6442a5e71e780b839c713141d171433864
mo91dev <>
2023-12-21 19:53:22 -0500

    first commit

--- vast2.sh
+++ vast2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-# After restaring the instance
 cd /home
 
 mkdir models 
-cd models 
 pip install "huggingface_hub[cli]"
-huggingface-cli download TheBloke/Pygmalion-2-13B-GGUF pygmalion-2-13b.Q5_K_M.gguf --local-dir . --local-dir-use-symlinks False
+
+# After restaring the instance, install kobold and model
+bash /home/koboldcpp/koboldcpp.sh &
+huggingface-cli download TheBloke/Pygmalion-2-13B-GGUF pygmalion-2-13b.Q5_K_M.gguf --local-dir /home/models --local-dir-use-symlinks False 
+wait
