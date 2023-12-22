5239507fdd2afd8766f47e06fb6031f87134723d
mo91dev <>
2023-12-21 20:11:46 -0500

    first commit

--- vast3.sh
+++ vast3.sh
@@ -2,4 +2,4 @@
 
 # in kobold folder
 cd /home/koboldcpp
-python ./koboldcpp.py --model /home/models/pygmalion-2-13b.Q5_K_M.gguf --gpulayers 100 --remotetunnel --smartcontext
+python ./koboldcpp.py --model /home/models/pygmalion-2-13b.Q5_K_M.gguf --gpulayers 100 --remotetunnel --usecublas
