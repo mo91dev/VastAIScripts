13de2ee1000654eda9473b3882a735389b2ffa75
mo91dev <>
2023-12-24 21:24:56 -0500

    first commit

--- vast2.sh
+++ vast2.sh
@@ -5,6 +5,7 @@ mkdir models
 pip install "huggingface_hub[cli]"
 
 # After restaring the instance, install kobold and model
-bash /root/koboldcpp/koboldcpp.sh &
+cd /root/koboldcpp
+./koboldcpp.sh &
 huggingface-cli download TheBloke/Mythalion-13B-GGUF mythalion-13b.Q5_K_M.gguf --local-dir /root/models --local-dir-use-symlinks False 
 wait
